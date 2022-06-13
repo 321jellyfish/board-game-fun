08e9c22b0d7e7ae51ee5974b573a3c11f3254b0d
321jellyfish <csljeffreys@gmail.com>
2022-06-13 12:07:16 +0100

    create utils folder and set up axios

--- src/components/reviewlist.js
+++ src/components/reviewlist.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchCategories } from "../utils/api";
 
 const ReviewList = () => {
   const [reviews, setReviews] = useState([
