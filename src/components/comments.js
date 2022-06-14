dbd2af46e0f6c771029b0c8282e1ed516ae239b6
321jellyfish <csljeffreys@gmail.com>
2022-06-14 16:55:03 +0100

    add conditionally rendered comment form

--- src/components/comments.js
+++ src/components/comments.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 const Comments = () => {
   const { reviewid } = useParams();
   const [comments, setComments] = useState([]);
+  const [addCommentStatus, setAddCommentStatus] = useState(false);
 
   useEffect(() => {
     fetchComments(reviewid).then((fetchedComments) => {
@@ -16,9 +17,54 @@ const Comments = () => {
     });
   }, [reviewid]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit form");
+  };
+
+  const selectAddComment = (event) => {
+    event.preventDefault();
+    console.log("select add comment");
+    setAddCommentStatus(true);
+  };
+
+  const returnToComments = (event) => {
+    event.preventDefault();
+    setAddCommentStatus(false);
+  };
+
+  if (addCommentStatus) {
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="comment-body">Your comment:</label>
+          <textarea
+            id="comment-body"
+            name="comment-body"
+            placeholder="Your comment"
+            rows="4"
+            cols="30"
+          />
+          <button>Submit comment</button>
+        </form>
+        <button onClick={returnToComments}>Return to comments</button>
+      </>
+    );
+  }
   return (
     <>
       <h3>Comments</h3>
+      <button onClick={selectAddComment}>Add comment</button>
+      {/* <form onSubmit={handleSubmit}>
+        <label for="comment-body">Comment:</label>
+        <input
+          type="textarea"
+          id="comment-body"
+          name="comment-body"
+          placeholder="Your comment"
+        />
+        <button>Submit comment</button>
+      </form> */}
       {comments ? (
         comments.map(({ body, author, comment_id }) => {
           return (
