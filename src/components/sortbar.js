0521632b2b3e5d2f24b764a4f2f7afb86996ee75
321jellyfish <csljeffreys@gmail.com>
2022-06-15 12:27:14 +0100

    set up drop down sort choice

--- src/components/sortbar.js
+++ src/components/sortbar.js
@@ -1,5 +1,20 @@
 const SortBar = () => {
-  return <p>Sort Bar</p>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form className="sort-form" onSubmit={handleSubmit}>
+      <label htmlFor="sort">Sort by:</label>
+      <select name="sort" id="sort">
+        <option>none</option>
+        <option>Date</option>
+        <option>Comment count</option>
+        <option>Votes</option>
+      </select>
+
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default SortBar;
