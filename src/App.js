df23e0cac453672aaa98a3427c2358dd6c11a01d
321jellyfish <csljeffreys@gmail.com>
2022-06-15 10:13:29 +0100

    create context for hardcoded logged in user

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from "./components/header";
 import ReviewList from "./components/reviewlist";
 import Nav from "./components/nav";
 import Review from "./components/review";
+import { UserContext } from "./context/user";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState("bainesface");
   return (
-    <BrowserRouter>
-      <Header />
-      <Nav />
-      <Routes>
-        <Route path="/" element={<ReviewList />}></Route>
-        <Route path="/:pathcategory" element={<ReviewList />}></Route>
-        <Route path="/:pathcategory/:reviewid" element={<Review />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<ReviewList />}></Route>
+          <Route path="/:pathcategory" element={<ReviewList />}></Route>
+          <Route path="/:pathcategory/:reviewid" element={<Review />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
