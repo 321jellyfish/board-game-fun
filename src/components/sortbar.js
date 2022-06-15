4de47ab127a5e0884424b9a0443af70c6dcaa82b
321jellyfish <csljeffreys@gmail.com>
2022-06-15 15:40:29 +0100

    set up asc/desc button and change text on click

--- src/components/sortbar.js
+++ src/components/sortbar.js
@@ -4,6 +4,7 @@ const SortBar = ({ searchParams, setSearchParams }) => {
   const [currentlySelectedSortBy, setCurrentlySelectedSortBy] =
     useState("created_at");
   const [submittedSortBy, setSubmittedSortBy] = useState("created_at");
+  const [order, setOrder] = useState("Descending");
 
   useEffect(() => {
     setSearchParams({ sort_by: submittedSortBy });
@@ -14,22 +15,35 @@ const SortBar = ({ searchParams, setSearchParams }) => {
     setSubmittedSortBy(currentlySelectedSortBy);
   };
 
-  return (
-    <form className="sort-form" onSubmit={handleSubmit}>
-      <label htmlFor="sort">Sort by:</label>
-      <select
-        name="sort"
-        id="sort"
-        onChange={(event) => {
-          setCurrentlySelectedSortBy(event.target.value);
-        }}
-      >
-        <option value="created_at">Date</option>
-        <option value="votes">Votes</option>
-      </select>
+  const handleOrderChange = (event) => {
+    event.preventDefault();
+    if (order === "Descending") {
+      setOrder("Ascending");
+    }
+    if (order === "Ascending") {
+      setOrder("Descending");
+    }
+  };
 
-      <button>Submit</button>
-    </form>
+  return (
+    <>
+      <form className="sort-form" onSubmit={handleSubmit}>
+        <label htmlFor="sort">Sort by:</label>
+        <select
+          name="sort"
+          id="sort"
+          onChange={(event) => {
+            setCurrentlySelectedSortBy(event.target.value);
+          }}
+        >
+          <option value="created_at">Date</option>
+          <option value="votes">Votes</option>
+        </select>
+
+        <button>Submit</button>
+      </form>
+      <button onClick={handleOrderChange}>{order}</button>
+    </>
   );
 };
 
