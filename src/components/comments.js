c565e67e88be3702fded920fcd2a6029c93cb1e7
321jellyfish <csljeffreys@gmail.com>
2022-06-15 10:17:14 +0100

    refactor when comment form appears

--- src/components/comments.js
+++ src/components/comments.js
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 const Comments = () => {
   const { reviewid } = useParams();
   const [comments, setComments] = useState([]);
-  const [addCommentStatus, setAddCommentStatus] = useState(false);
 
   useEffect(() => {
     fetchComments(reviewid).then((fetchedComments) => {
@@ -23,56 +22,19 @@ const Comments = () => {
     console.log("submit form");
   };
 
-  const selectAddComment = (event) => {
-    event.preventDefault();
-    setAddCommentStatus(true);
-  };
-
-  const returnToComments = (event) => {
-    event.preventDefault();
-    setAddCommentStatus(false);
-  };
-
-  if (addCommentStatus) {
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="comment-body">Your comment:</label>
-          <textarea
-            id="comment-body"
-            name="comment-body"
-            placeholder="Your comment"
-            rows="4"
-            cols="30"
-          />
-          <button>Submit comment</button>
-        </form>
-        <button onClick={returnToComments}>Return to comments</button>
-        <h3>Comments</h3>
-        <button onClick={selectAddComment}>Add comment</button>
-
-        {comments ? (
-          comments.map(({ body, author, comment_id }) => {
-            return (
-              <div className="comment-card">
-                <p>{body}</p>
-                <p className="commenter">
-                  <span className="bold">Commenter: </span>
-                  {author}
-                </p>
-              </div>
-            );
-          })
-        ) : (
-          <p className="no-comments">No comments yet...</p>
-        )}
-      </>
-    );
-  }
   return (
     <>
       <h3>Comments</h3>
-      <button onClick={selectAddComment}>Add comment</button>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          id="comment-body"
+          name="comment-body"
+          rows="4"
+          cols="30"
+          placeholder="Your comment"
+        />
+        <button>Submit comment</button>
+      </form>
 
       {comments ? (
         comments.map(({ body, author, comment_id }) => {
