a8cca54902c35f863c4d29038926e65a0eac6d39
321jellyfish <csljeffreys@gmail.com>
2022-06-15 16:36:35 +0100

    moved logged in user display

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,17 @@ import { UserContext } from "../context/user";
 
 const Header = () => {
   const { user } = useContext(UserContext);
+
   return (
     <header>
+      <p>
+        <span className="bold">Logged in as: </span>
+        {user}
+      </p>
       <h1>
         <Link to="/">Board Game Fun</Link>
       </h1>
       <p>Rate, discuss and find out about your favourite board games</p>
-      <p>
-        <span className="bold">Logged in as:</span> {user}
-      </p>
     </header>
   );
 };
