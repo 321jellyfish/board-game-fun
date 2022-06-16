56709a3ee5641033c6156869bb74f031b8e01de7
321jellyfish <csljeffreys@gmail.com>
2022-06-16 11:28:48 +0100

    add link to homepage to error page

--- src/components/errorpage.js
+++ src/components/errorpage.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <p>This page does not exist</p>;
+  return (
+    <>
+      <p>
+        <strong>This page does not exist</strong>
+      </p>
+      <Link to="/">Take me back to all reviews</Link>
+    </>
+  );
 };
 
 export default ErrorPage;
